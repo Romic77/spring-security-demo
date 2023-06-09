4686ab75d91c70653a8065348a6549a9ad21b278
Romic <280385556@qq.com>
2023-06-09 18:13:25 +0800

    提交

---
+++ src/main/java/com/example/springsecuritydemo/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Romic
+ * @date 2023/6/9
+ * @description
+ */
+@Configuration
+public class SecurityConfig {
+    
+}
---
+++ src/main/java/com/example/springsecuritydemo/filter/DynamicSecurityFilter.java
@@ -0,0 +1,36 @@
+package com.example.springsecuritydemo.filter;
+
+import org.springframework.security.access.SecurityMetadataSource;
+import org.springframework.security.access.intercept.AbstractSecurityInterceptor;
+import org.springframework.security.web.FilterInvocation;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Romic
+ * @date 2023/6/9
+ * @description
+ */
+public class DynamicSecurityFilter extends AbstractSecurityInterceptor implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public Class<?> getSecureObjectClass() {
+        return FilterInvocation.class;
+    }
+
+    @Override
+    public SecurityMetadataSource obtainSecurityMetadataSource() {
+        return null;
+    }
+}
